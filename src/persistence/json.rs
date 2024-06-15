2eeb6b1f0bc2ca6046e4f74ca46eda7daff2a6d1
Jonas Hansen <jonas@niceapps.ch>
2024-06-15 22:53:09 +0200

    Better (not smaller) JSON encoder

--- src/persistence/json.rs
+++ src/persistence/json.rs
@@ -3,9 +3,8 @@ use std::io::Read;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-use crate::{coords, model};
 use crate::image::Coords;
-use crate::model::{Compressed, Transformation};
+use crate::{coords, model};
 
 #[derive(Error, Debug)]
 pub enum SerializationError {
@@ -13,7 +12,7 @@ pub enum SerializationError {
     Serialization(#[from] serde_json::Error),
 }
 
-pub fn serialize(compressed: &Compressed) -> Result<Vec<u8>, SerializationError> {
+pub fn serialize(compressed: &model::Compressed) -> Result<Vec<u8>, SerializationError> {
     let contents = Contents::from(compressed.clone());
     let serialized = serde_json::to_string(&contents)?;
     Ok(serialized.into_bytes())
@@ -24,63 +23,80 @@ pub enum DeserializationError {
     Deserialization(#[from] serde_json::Error),
 }
 
-pub fn deserialize(reader: impl Read) -> Result<Compressed, DeserializationError> {
+pub fn deserialize(reader: impl Read) -> Result<model::Compressed, DeserializationError> {
     let contents: Contents = serde_json::from_reader(reader)?;
     let transformations = contents
-        .m
+        .mappings
         .into_iter()
-        .map(|(domain, range, brightness, saturation)| Transformation {
+        .map(|x| model::Transformation {
             range: model::Block {
-                block_size: range.0,
-                image_size: contents.w,
-                origin: coords!(range.1, range.2),
+                block_size: x.range.size,
+                image_size: contents.size,
+                origin: coords!(x.range.x, x.range.y),
             },
             domain: model::Block {
-                block_size: domain.0,
-                image_size: contents.w,
-                origin: coords!(domain.1, domain.2),
+                block_size: x.domain.size,
+                image_size: contents.size,
+                origin: coords!(x.domain.x, x.domain.y),
             },
-            brightness,
-            saturation,
+            brightness: x.brightness,
+            saturation: x.saturation,
         })
         .collect();
 
-    Ok(Compressed(transformations))
+    Ok(model::Compressed(transformations))
 }
 
 #[derive(Serialize, Deserialize)]
 struct Contents {
-    w: u32,
-    m: Vec<(Block, Block, i16, f64)>,
+    size: u32,
+    mappings: Vec<Mapping>,
 }
 
-impl From<Compressed> for Contents {
-    fn from(value: Compressed) -> Self {
+impl From<model::Compressed> for Contents {
+    fn from(value: model::Compressed) -> Self {
         let transformations = value.0;
         // TODO: No!
         let image_size = transformations[0].domain.image_size;
         Self {
-            w: image_size,
-            m: transformations
-                .into_iter()
-                .map(|t| {
-                    (
-                        Block::from(t.domain),
-                        Block::from(t.range),
-                        t.brightness,
-                        t.saturation,
-                    )
-                })
-                .collect(),
+            size: image_size,
+            mappings: transformations.into_iter().map(Mapping::from).collect(),
         }
     }
 }
 
 #[derive(Serialize, Deserialize)]
-struct Block(u32, u32, u32);
+struct Mapping {
+    domain: Block,
+    range: Block,
+    brightness: i16,
+    saturation: f64,
+}
 
-impl From<crate::model::Block> for Block {
-    fn from(value: crate::model::Block) -> Self {
-        Self(value.block_size, value.origin.x, value.origin.y)
+impl From<model::Transformation> for Mapping {
+    fn from(value: model::Transformation) -> Self {
+        Self {
+            domain: Block::from(value.domain),
+            range: Block::from(value.range),
+            brightness: value.brightness,
+            saturation: value.saturation,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Block {
+    size: u32,
+    x: u32,
+    y: u32,
+}
+
+impl From<model::Block> for Block {
+    fn from(value: model::Block) -> Self {
+        Self {
+            size: value.block_size,
+            x: value.origin.x,
+            y: value.origin.y,
+        }
     }
 }
